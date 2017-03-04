87b81cac3255c0d13c4a78798082a1072525b114
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-03-04 17:05:54 -0500

    Refactor isAlive state to be 1/0 instead of true/false for counting neighbors

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -10,27 +10,30 @@ class Cell extends Component {
 	}
 
 	toggleAlive() {
-		if(this.state.isAlive == true) {
-			this.setState({ isAlive: false });
+		if(this.state.isAlive == 1) {
+			this.setState({ isAlive: 0 });
 			console.log("cell is now dead");
 			this.props.updateState(this.props.rowNum, this.props.colNum, this.state.isAlive);
 		}
 		else {
-			this.setState({ isAlive: true });
+			this.setState({ isAlive: 1 });
 			console.log("cell is now alive");
 			this.props.updateState(this.props.rowNum, this.props.colNum, this.state.isAlive);
 		}
+
+		// For testing neighbor count
+		this.props.countAliveNeighbors(this.props.rowNum, this.props.colNum);
 	}
 
 	render(){
-		if(this.state.isAlive == true) {
+		if(this.state.isAlive == 1) {
 			return (
 				<div id="board-cell">
 				  <input className="alive" type="button" onClick={this.toggleAlive} />
 				</div>
 			);
 		}
-		else if(this.state.isAlive == false) {
+		else if(this.state.isAlive == 0) {
 			return (
 				<div id="board-cell">
 				  <input className="notAlive" type="button" onClick={this.toggleAlive} />
--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -13,6 +13,12 @@ class GameBoard extends Component {
 			boardRowAliveState = [];
 			for(var j=0; j<this.props.numOfCols; j++) {
 				randStartingCellState = Math.random() >= 0.5;
+				if(randStartingCellState == true) {
+					randStartingCellState = 1;
+				}
+				else {
+					randStartingCellState = 0;
+				}
 				boardRowAliveState.push(randStartingCellState);
 			}
 			initialBoardState.push(boardRowAliveState);
@@ -21,14 +27,79 @@ class GameBoard extends Component {
 		this.state = { currentBoardState: initialBoardState, nextBoardState: [] }
 
 		this.updateBoardState = this.updateBoardState.bind(this);
+		this.getNextGenerationCellValues = this.getNextGenerationCellValues.bind(this);
+		this.getAliveNeighborCount = this.getAliveNeighborCount.bind(this);
 	}
 
 	updateBoardState(cellRow, cellCol, newStateValue) {
-		console.log("updating board state...");
+		//console.log("updating board state...");
 		var updatedBoard = this.state.currentBoardState;
 		updatedBoard[cellRow][cellCol] = newStateValue;
 
 		this.setState({ currentBoardState: updatedBoard });
+	}
+
+	getNextGenerationCellValues() {
+
+	}
+
+	getAliveNeighborCount(cellRow, cellCol) {
+		var currentBoard = this.state.currentBoardState;
+		//console.log(currentBoard);
+		var numNeighborsAlive = 0;
+		console.log("r: " + cellRow + ", c: " + cellCol);
+
+		//Top left corner, no row above and no column to left
+		if(cellRow == 0 && cellCol == 0) {
+			console.log("top left corner");
+			numNeighborsAlive = currentBoard[cellRow][cellCol+1]
+							  + currentBoard[cellRow+1][cellCol]
+							  + currentBoard[cellRow+1][cellCol+1];
+		}
+		//Top right corner, no row above and no column to right
+		else if(cellRow == 0 && cellCol == (this.props.numOfCols - 1)) {
+			console.log("top right corner");
+			numNeighborsAlive = currentBoard[cellRow][cellCol-1]
+							  + currentBoard[cellRow+1][cellCol-1]
+							  + currentBoard[cellRow+1][cellCol];
+		}
+		//Bottom left corner, no row below and no column to left
+		else if(cellRow == (this.props.numOfRows - 1) && cellCol == 0) {
+			console.log("bottom left corner");
+			numNeighborsAlive = currentBoard[cellRow][cellCol+1]
+							  + currentBoard[cellRow-1][cellCol]
+							  + currentBoard[cellRow-1][cellCol+1];
+		}
+		//Bottom right corner, no row below and no column to right
+		else if(cellRow == (this.props.numOfRows - 1) && cellCol == (this.props.numOfCols - 1)) {
+			console.log("bottom right corner");
+			numNeighborsAlive = currentBoard[cellRow][cellCol-1]
+							  + currentBoard[cellRow-1][cellCol]
+							  + currentBoard[cellRow-1][cellCol-1];
+		}
+		//If on first row, no row above
+		else if(cellRow == 0) {
+			numNeighborsAlive = currentBoard[cellRow][cellCol];
+		}
+		//If on left most column, no column to left
+		else if(cellCol == 0) {
+
+		}
+		//If on last row, no row below
+		else if(cellRow == this.props.numOfRows) {
+
+		}
+		//If on right most column, no column to right
+		else if(cellCol == this.props.numOfCols) {
+
+		}
+		//Interior cells
+		else {
+
+		}
+
+		console.log(numNeighborsAlive);
+
 	}
 
 	render() {
@@ -42,7 +113,8 @@ class GameBoard extends Component {
 		for(var i=0; i<numRows; i++) {
 			boardRow = [];
 			for(var j=0; j<numCols; j++) {
-				boardRow.push(<Cell rowNum={i} colNum={j} updateState={this.updateBoardState} aliveState={this.state.currentBoardState[i][j]} />);
+				boardRow.push(<Cell rowNum={i} colNum={j} updateState={this.updateBoardState} 
+					aliveState={this.state.currentBoardState[i][j]} countAliveNeighbors={this.getAliveNeighborCount} />);
 			}
 			gameBoard.push(<div id="board-row">{boardRow}</div>);
 		}
