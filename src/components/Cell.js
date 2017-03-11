5d5b46bd335664a6b4c8cb2129b17dd15bc65056
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-03-11 12:25:08 -0500

    Fix bug with manually changing cell alive status

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -13,34 +13,34 @@ class Cell extends Component {
 		if(this.state.isAlive == 1) {
 			this.setState({ isAlive: 0 });
 			console.log("cell is now dead");
-			this.props.updateState(this.props.rowNum, this.props.colNum, this.state.isAlive);
+			this.props.updateState(this.props.rowNum, this.props.colNum, 0);
 		}
 		else {
 			this.setState({ isAlive: 1 });
 			console.log("cell is now alive");
-			this.props.updateState(this.props.rowNum, this.props.colNum, this.state.isAlive);
+			this.props.updateState(this.props.rowNum, this.props.colNum, 1);
 		}
 
 		// For testing neighbor count
-		this.props.countAliveNeighbors(this.props.rowNum, this.props.colNum);
+		//this.props.countAliveNeighbors(this.props.rowNum, this.props.colNum);
 	}
 
 	componentWillUpdate() {
 		// Check if cell needs to be rendered with an updated value
 		if(this.state.isAlive != this.props.aliveState) {
-			this.setState({ isAlive: this.props.aliveState });
+			this.setState({ isAlive: this.props.aliveState }, function() { this.forceUpdate() });
 		}
 	}
 
 	render(){
-		if(this.state.isAlive == 1) {
+		if(this.props.aliveState == 1) {
 			return (
 				<div id="board-cell">
 				  <input className="alive" type="button" onClick={this.toggleAlive} />
 				</div>
 			);
 		}
-		else if(this.state.isAlive == 0) {
+		else if(this.props.aliveState == 0) {
 			return (
 				<div id="board-cell">
 				  <input className="notAlive" type="button" onClick={this.toggleAlive} />
